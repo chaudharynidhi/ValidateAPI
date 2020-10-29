5ae18a224dc78a26d709361d0c16c646cd872f3a
Nidhi Chaudhary <mynetworknidhi02@gmail.com>
2020-10-30 00:44:19 +0530

    Update views.py

--- FormAPP/views.py
+++ FormAPP/views.py
@@ -3,23 +3,29 @@ import json
 import requests
 # Create your views here.
 
+
+# for viewing the validation for ID Api
 def id_index(request):
     resp = requests.get('http://127.0.0.1:8000/api/id/')
     data = resp.json()
     lidi = []
+    
+    # looping for the data came from the API and creating the validation response based on the inputs received in API.
     for da in data:
         print(len(da['values']))
         di={'filled':False, 'partially_filled':False, 'trigger':da['invalid_trigger'], 
-        'parameters':{}}
+        'parameters':{}}    # Initialization of Dictionary which will be further appended for the response parameters
         li=[]
-        st=''
         val = da['values']
+        
+        # Creation for the valid response when the multiple values are allowed
         if da['support_multiple']==True:
             for i in range(len(val)):
                 if val[i]['entity_name']==da['type1'][0]:
                     for d in da['support_values']:
                             if(d==val[i]['value']):
                                     li.append(d)  
+            # creating the valid dictionary based on the values feild in the JSON response                        
             if len(li)==0:
                 di= {'filled':False, 'partially_filled':True, 'trigger':da['invalid_trigger'], 
                 'parameters':{}}
@@ -33,6 +39,7 @@ def id_index(request):
                 'parameters':{da['key']: li}} 
             lidi.append(di)    
 
+        # Creation for the valid response when the multiple values are not allowed an donly first one has to be picked    
         elif da['pick_first']==True: 
             for i in range(len(val)):
                 if val[i]['entity_name']==da['type1'][0]:
@@ -54,24 +61,31 @@ def id_index(request):
         print(di)
     return render(request, 'id/index.html', {'list': lidi})
 
+
+
+# for viewing the validation for Age Api
 def age_index(request):
     resp = requests.get('http://127.0.0.1:8000/api/age/')
     data = resp.json()
     lidi = []
-
+    
+    # looping for the data came from the API and creating the validation response based on the inputs received in API.
     for da in data:
         print(len(da['values']))
         di={'filled':False, 'partially_filled':False, 'trigger':da['invalid_trigger'], 
-        'parameters':{}}
+        'parameters':{}}   # Initialization of Dictionary which will be further appended for the response parameters
         li=[]
-        st=''
         val = da['values']
+        
+         # Creation for the valid response when the multiple values are allowed
         if da['support_multiple']==True:
             for i in range(len(val)):
                 if val[i]['entity_type']==da['type1'][0]:
                     exec("%s = %d" % (da['var_name'],val[i]['value']))
                     if(eval(da['constraint'])):
                         li.append(val[i]['value'])  
+                        
+                # creating the valid dictionary based on the values feild in the JSON response 
                 if len(li)==0:
                     di= {'filled':False, 'partially_filled':True, 'trigger':da['invalid_trigger'], 
                     'parameters':{}}
@@ -84,7 +98,8 @@ def age_index(request):
                     di= {'filled':True, 'partially_filled':False, 'trigger':'', 
                     'parameters':{da['key']: li}} 
             lidi.append(di)    
-
+    
+        # Creation for the valid response when the multiple values are not allowed an donly first one has to be picked    
         elif da['pick_first']==True: 
             for i in range(len(val)):
                 if val[i]['entity_type']==da['type1'][0]:
